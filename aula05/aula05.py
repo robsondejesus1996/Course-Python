ffe38a54499f89bd5398fdad796731786078de45
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-04-11 13:32:11 -0300

    Operadores Aritméticos

---
+++ aula05/aula05.py
@@ -0,0 +1,16 @@
+print(10 * 10)
+print(10 + 10)
+print(10 - 5)
+print(10 / 2)
+
+
+print('Robson' + ' ' + 'de Jesus ' + str(27) + ' anos')
+
+print(10.5 // 3)
+print(10 / 3)
+
+print(2**10)
+print(10 % 3)
+
+print(5+2*10)
+print((5+2)*10)
