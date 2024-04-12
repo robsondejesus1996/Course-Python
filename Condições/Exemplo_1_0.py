55e12aa94bf91513c97e986e681459a57290d8ed
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-04-11 21:07:40 -0300

    Implementação de Condicionais

---
+++ Condições/Exemplo_1_0.py
@@ -0,0 +1,10 @@
+if False:
+    print('Verdadeiro')
+elif True:
+    print("Agora é verdadeiro")
+    nome = input("Qual é o seu nome: ")
+
+    print(f'Seu nome é {nome}')
+else:
+    print('Não é Verdadeiro')
+
