bc0f6c40d45b4aea9d0192f8354b62d479c6d611
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-07-07 11:31:26 -0300

    feat: created routes post, get and get id tasks

--- Rocketseat/Project Tasks/app.py
+++ Rocketseat/Project Tasks/app.py
@@ -1,14 +1,44 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.tasks import Task
 
 app = Flask(__name__)
 
-@app.route("/")
-def helloWorld():
-    return "Hello world!"
+tasks = [] 
+task_id_control = 1
 
-@app.route("/about")
-def about():
-    return "Pagina sobre"
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id= task_id_control, title=data["title"], description=data.get("description", ""))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+
+    return  jsonify({"message" : "Nova tarefa criada com sucesso"})
+
+@app.route('/tasks', methods=['GET'])
+def get_task():
+    task_list = [task.to_dict() for task in tasks]
+    
+
+
+    output = {
+            "tasks": task_list,
+            "total_tasks": len(task_list)
+            }
+    return jsonify(output)
+
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_tasks(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+    
+    return jsonify({"message": "Não foi possível encontrar a atividade"}), 400
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
