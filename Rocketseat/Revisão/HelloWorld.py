03cf38b47af220e56c61279366b5c3beaf558ee4
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-12-26 17:28:03 -0300

    feat: update 2024/2025

---
+++ Rocketseat/Revisão/HelloWorld.py
@@ -0,0 +1,1 @@
+print('Hello World Robson de Jesus')
---
+++ Rocketseat/Revisão/Modulo_1/sintaxe.py
@@ -0,0 +1,13 @@
+# Comentario de 1 linha
+
+"""
+Várias linhas
+Várias linhas
+Várias linhas
+Várias linhas
+"""
+
+nome = "Robson" + \
+    "De Jesus"
+
+print('Olá mundo com o desenvolvedor', nome)
